d3fd082e12d51578af42cacfc3d84ad85db19e69
iamyosuke <murata@open-dna.jp>
2024-07-14 18:56:42 +0900

    blogapp

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,35 @@
 import styles from "./homepage.module.css";
 
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <Link href="/">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ut tempore
+      veritatis doloribus, molestiae maiores impedit, fugit fuga, non modi nemo
+      iure quibusdam ratione minus beatae archiLorem ipsum dolor sit amet
+      consectetur adipisicing elit. Libero ut tempore veritatis doloribus,
+      molestiae maiores impedit, fugit fuga, non modi nemo iure quibusdam
+      ratione minus beatae architecto. Dignissimos, Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Libero ut tempore veritatis doloribus,
+      molestiae maiores impedit, fugit fuga, non modi nemo iure quibusdam
+      ratione minus beatae architecto. Dignissimos, Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Libero ut tempore veritatis doloribus,
+      molestiae maiores impedit, fugit fuga, non modi nemo iure quibusdam
+      ratione minus beatae architecto. Dignissimos, Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Libero ut tempore veritatis doloribus,
+      molestiae maiores impedit, fugit fuga, non modi nemo iure quibusdam
+      ratione minus beatae architecto. Dignissimos, Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Libero ut tempore veritatis doloribus,
+      molestiae maiores impedit, fugit fuga, non modi nemo iure quibusdam
+      ratione minus beatae architecto. Dignissimos, Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Libero ut tempore veritatis doloribus,
+      molestiae maiores impedit, fugit fuga, non modi nemo iure quibusdam
+      ratione minus beatae architecto. Dignissimos, Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Libero ut tempore veritatis doloribus,
+      molestiae maiores impedit, fugit fuga, non modi nemo iure quibusdam
+      ratione minus beatae architecto. Dignissimos, tecto. Dignissimos, facere
+      eius?
+    </Link>
+  );
 }
